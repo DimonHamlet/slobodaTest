ee110e222769c966e03cce77de139327e4d03be4
Dima <formyblogger69@gmail.com>
2020-03-30 23:08:33 +0300

    Sloboda Studio Task01 Candies

---
+++ candies.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+puts 'Enter X(candy weight)'
+x = gets.chomp.to_i
+puts 'Enter Y(tangerine weight)'
+y = gets.chomp.to_i
+puts 'Enter Z(apple weight)'
+z = gets.chomp.to_i
+puts 'Enter W(total weight)'
+w = gets.chomp.to_i
+
+variations = 0
+last = w + 1
+
+# Solution for preferably small data because of algorithm time-efficiency
+(0..last).step(x).each do |candy|
+  (0..last).step(y).each do |tangerine|
+    (0..last).step(z).each do |apple|
+      sum = candy + tangerine + apple
+      variations += 1 if sum == w
+    end
+  end
+end
+
+puts("Maximum gift options - #{variations}")
